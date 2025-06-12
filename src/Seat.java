65fe01905d2fbf87115916857734095cce12e1b8
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-06-12 15:45:16 -0230

    Set up the basic seat functions

--- src/Seat.java
+++ src/Seat.java
@@ -1,2 +1,33 @@
 public class Seat {
+    public class Seat {
+        private String seatId;
+        private boolean isReserved;
+
+        public Seat(String seatId) {
+            this.seatId = seatId;
+            this.isReserved = false;
+        }
+
+        public String getSeatId() {
+            return seatId;
+        }
+
+        public boolean isReserved() {
+            return isReserved;
+        }
+
+        public void reserve() {
+            isReserved = true;
+        }
+
+        public void cancelReservation() {
+            isReserved = false;
+        }
+
+        @Override
+        public String toString() {
+            return isReserved ? "[X]" : "[ ]";
+        }
+    }
+
 }
